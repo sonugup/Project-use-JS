45d71b8f074625e601a25adf66b20904491d2b5f
Sonu Gupta <rg3029926@gmail.com>
2023-02-11 19:41:05 +0530

    clock

--- project-1/index.js
+++ project-1/index.js
@@ -1,32 +1,32 @@
-const hourel=document.getElementById('hour')
-const mint=document.getElementById("minutes")
-const sec=document.getElementById('seconds')
+const hourel = document.getElementById("hour");
+const mint = document.getElementById("minutes");
+const sec = document.getElementById("seconds");
 
-const ampm=document.getElementById('ampm')
+const ampm = document.getElementById("ampm");
 
+function updateClock() {
+  let h = new Date().getHours();
+  let m = new Date().getMinutes();
+  let s = new Date().getSeconds();
+  let ampm = "AM";
 
-function updateClock(){
-    let h=new Date().getHours();
-    let m=new Date().getMinutes();
-    let s=new Date().getSeconds();
-    let ampm="AM"
+  if (h > 12) {
+    h = h - 12;
+    ampm = "PM";
+  }
 
-    if(h>12){
-        h=h-12;
-        ampm="PM";
-    }
+  h = h < 10 ? "0" + h : h;
+  m = m < 10 ? "0" + m : m;
+  s = s < 10 ? "0" + s : s;
 
-    h=h<10? "0" + h:h;
-    m=m<10? "0" + m:m;
-    s=s<10? "0" + s:s;
+  hourel.innerText = h;
+  mint.innerText = m;
+  sec.innerText = s;
+  ampm, (innerText = ampm);
 
-    hourel.innerText=h;
-    mint.innerText= m;
-    sec.innerText=s;
-    ampm, (innerText=ampm);
-
-    setTimeout(() => {
-        updateClock()
-    }, 1000)
+  setTimeout(() => {
+    updateClock();
+  }, 1000);
 }
-updateClock()
+updateClock();
+
